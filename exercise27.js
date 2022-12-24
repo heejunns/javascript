dfbcf857e524ecaa52ff8642c097e78275d8425f
heejunns <wns2252@hanmail.net>
2022-12-24 22:48:38 +0900

    삼항 연산자, switch 문

---
+++ exercise27.js
@@ -0,0 +1,29 @@
+// 삼항 연산자
+
+let x = 2;
+
+let result = x%2 ? "홀수 입니다." : "짝수 입니다.";
+
+console.log(result);
+
+////// switch 문 
+
+let x1 = 10;
+
+switch(x1){
+   case 1:
+    console.log("안녕하세요 1 입니다.");
+    break;
+    case 2:
+        console.log("안녕하세요 2 입니다.");
+        break;
+    case 3:
+        console.log("인녕하세요 3 입니다.");
+        break;
+    default:
+        console.log("해당하는 숫자가 없네요.")
+}
+
+
+
+
