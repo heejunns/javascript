32a2527e41deca3e20618e82432ae3a07db01b0d
heejunns <wns2252@hanmail.net>
2023-01-28 19:40:54 +0900

    Array.isArray() 사용해보기

---
+++ 자바스크립트 연습코드/exercise12.js
@@ -0,0 +1,8 @@
+// Array.isArray() 사용해보기
+let arr = [1,2,3,4,5,6,7,8,9,10];
+
+
+let result7 = Array.isArray(arr);
+
+console.log("Array.isArray() 사용 결과 : " +result7);
+console.log("typeof 사용 결과 : "+typeof arr); // object 출력
