6b10bcd872803e847ea7e6ada5cf0b5c71a514ed
heejunns <wns2252@hanmail.net>
2023-02-06 21:01:39 +0900

    반응속도 테스트 js

--- 반응속도 테스트/response-check-code.js
+++ 반응속도 테스트/response-check-code.js
@@ -3,13 +3,14 @@ const $restart = document.querySelector('#restart');
 
 let startTime; // 화면이 연두색으로 바뀔때 시간을 기록하는 변수
 let endTime; // 연두색인 화면을 클릭했을때의 시간을 기록하는 변수
+let records = [];
 const clickScreen = () => {
   // 화면 클릭을 하면 호출되는 함수
   if ($screen.className === 'wait') {
-    // 화면이 빨간색일때 클릭한다ㅕㄴ
+    // 화면이 빨간색일때 클릭한다
     $screen.classList.replace('wait', 'prepare'); // prepare 로 클래스 이름 변경
     $screen.textContent = '곧 반응속도 테스트가 시작 됩니다!';
-    setInterval(() => {
+    setTimeout(() => {
       if ($screen.className === 'prepare') {
         // 화면이 파란색이라면
         $screen.classList.replace('prepare', 'start'); // start 로 클래스 이름 변경
@@ -26,7 +27,16 @@ const clickScreen = () => {
   } else if ($screen.className == 'start') {
     // 화면이 연두색일때 클릭한다면
     endTime = new Date(); // 클릭 시간 기록
-    $screen.textContent = `반응 시간 :  ${endTime - startTime} ms`;
+    const record = endTime - startTime; // 반응속도
+    records.push(record); // 배열애 저장하기
+    const average =
+      records.reduce(function (a, b) {
+        // 배열의 모든 값을 더하고 배열의 길이로 나누어 평균 구하기
+        return a + b;
+      }) / records.length;
+    $screen.textContent = `반응 시간 :  ${record} ms , 평균 반응시간 : ${average} ms`; // 화면에 보여주기}
+    startTime = null;
+    endTime = null;
   }
 };
 
